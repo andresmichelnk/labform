2b89e5638e2880d3a58438496f7ca2373529d173
andresmichelnk <michelandrestamyo@gmail.com>
2021-07-15 07:30:37 -0400

    Mitad del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-list></form-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Formulario from './components/Formulario.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'form-list':Formulario
+  },
+  data(){
+    return{
+      
+    }
   }
 }
 </script>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <label>
+            <h4>Ingrese lo que le de la gana</h4>
+            <input type="text" v-model="texto.name">
+        </label>
+        <button @click="guardartext">Guardar</button>
+
+        <div >
+            <ul>
+                <h4>Lista</h4>
+                <li v-for="item of list" :key="item">
+                    <input type="checkbox" :value="item" v-model="list2"> 
+                    {{item}}
+                </li>
+            </ul>
+        </div>  
+
+        <div>
+            <ul>
+                <h4 v-if="list2 != null">Lista Guarda</h4>
+                <li v-for="item2 of list2" :key="item2">
+                   <!-- <input type="checkbox" > -->
+                    {{item2}}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { Texto } from '../utiles/Texto'
+
+ var texto = new Texto({name:"Dno",view:false})
+
+export default {
+    name: 'Formulario',
+    data(){
+        return{
+         texto,
+          list:[],
+          list2:[],
+         // guardar: false
+        
+        }
+        
+    },
+
+    methods:{
+        guardartext(){ 
+           this.list.push(this.texto.name) 
+           this.texto.name = ''
+         //  this.texto.view = false
+           console.log(this.list)
+           
+        },
+      
+    },
+
+ 
+}
+
+
+</script>
+
+<style>
+
+</style>
